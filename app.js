28384f134fad9b7b21cdf668b1cce56579975aaf
Saint. Clever <parchment@gmx.com>
2024-10-22 12:03:59 -0400

    Check to see if array is nested

--- app.js
+++ app.js
@@ -17,3 +17,17 @@ let nestedArray = [[0, [[1, 2], 3, [4, 5]], 6]];
 console.log(nestedArray.flat(Infinity));
 
 
+let arrayFlat = (arr) => {
+  for (i in arr) {
+    console.log(arr[i])
+  }
+  return arr;
+}
+
+
+let trampoline = (func) => {
+  let result = func
+  return result;
+}
+
+console.log(trampoline(arrayFlat(nestedArray)));
