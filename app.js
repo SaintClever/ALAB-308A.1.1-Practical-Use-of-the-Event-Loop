03c8c3bbe28e282f184b7cdce778d787bc792d1f
Saint. Clever <parchment@gmx.com>
2024-10-22 11:42:55 -0400

    Add flatten array example

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let counter = 0;
+// let counter = 0;
 
-try {
-  let simpleFunction = () => {
-    console.log(counter++);
-    simpleFunction();
-  }
-  simpleFunction();
-} catch(e) {
-  console.error(e);
-}
+// try {
+//   let simpleFunction = () => {
+//     console.log(counter++);
+//     simpleFunction();
+//   }
+//   simpleFunction();
+// } catch(e) {
+//   console.error(e);
+// }
+
+
+let nestedArray = [[0, [[1, 2], 3, [4, 5]], 6]];
+
+// Goal
+console.log(nestedArray.flat(Infinity));
+
+
